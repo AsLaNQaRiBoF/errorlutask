725aa1d3c39a7e6ec539e32faa9ca60edd7a9255
aslan <aslanqaribov7@gmail.com>
2023-04-03 18:40:51 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0 - İnformasiya almaq üçün");
+            Console.WriteLine("1 - Shoot metodu üçün");
+            Console.WriteLine("2 - Fire metodu üçün");
+            Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+            Console.WriteLine("4 - Reload metodu üçün");
+            Console.WriteLine("5 - ChangeFireMode metodu üçün");
+            Console.WriteLine("6 - Proqramdan dayandırmaq üçün\n");
+            Console.Write("Seciminizi edin: ");
+            
+            {
+                
+                int choose = int.Parse(Console.ReadLine());
+                while (true)
+                {
+                    switch (choose)
+                    {
+                        case 0:
+                            Weapon.information();
+                            break;
+                        case 1:
+                            Weapon.Shoot();
+                        break;
+                        case 2:
+                            Weapon.Fire();
+                            break;
+                        case 3:
+                            Weapon.GetRemainBulletCount();
+                            break;
+                        case 4:
+                            Weapon.Reload();
+                            break;
+                        case 5:
+                            Weapon.ChangeFireMode();
+                            break;
+                        case 6:
+                            Console.WriteLine("Goodbye");
+                            check = false;
+                            break;
+                        default:
+                            Console.WriteLine("Error")
+                            break;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    internal class Weapon
+    {
+        public int daraginGulleTutum;
+        public int daragdakiGulleSayi;
+        public int daraginBowalmaSaniyesi;
+        public bool otomatikAtiwModu = true;
+
+        public Weapon(int daraginGulleTutumu, int  daragdakigulleSayi, int daraginBowalmaSaniyesi, bool otomatikAtiwModu)
+
+
+
+
+        {
+            if (daraginGulleTutumu<=0 || daragdakiGulleSayi<=0 || daraginBowalmaSaniyesi<=0)
+            {
+                Console.WriteLine("Yanliw parametirler qeyd olunmuwdur");
+            }
+            this.daraginGulleTutum = daraginGulleTutumu;
+            this.daragdakiGulleSayi = daragdakiGulleSayi;
+            this.daraginBowalmaSaniyesi = daraginBowalmaSaniyesi;
+            this.otomatikAtiwModu = otomatikAtiwModu;
+        }
+        public void information()
+        {
+            Console.WriteLine($"Daragin gulle tutumu:{daraginGulleTutumu}");
+            Console.WriteLine($"Daragdaki gulle sayi:{daragdakiGulleSayi}");
+            Console.WriteLine($"Daragin bowalma saniyesi:{daraginBowalmaSaniyesi}");
+            Console.WriteLine($"Atiw modunun rejimi:{otomatikAtiwModu}");
+        }
+        public void Shoot()
+        {
+            if (otomatikAtiwModu == true)
+            {
+                if (daragdakiGulleSayi > 0)
+                {
+                    Console.WriteLine("Atew Edildi");
+                    daragdakiGulleSayi--;
+                }
+
+                else
+                {
+                    Console.WriteLine("Siz otomatik moddasiz");
+                }
+            }
+        }
+        public void Fire()
+        {
+            if (otomatikAtiwModu== false)
+            {
+                if (daragdakiGulleSayi > 0)
+                {
+                    Console.WriteLine($"Daraginizdaki gulle sayi:{(daragdakiGulleSayi / daraginGulleTutumu) * daraginBowalmaSaniyesi}");
+                    daragdakiGulleSayi = 0;
+
+                }
+                else
+                {
+                    Console.WriteLine("Daragda Gulle Yoxdur");
+                }
+            }
+        }
+        public void ChangeFireMode()
+        {
+            otomatikAtiwModu = !otomatikAtiwModu;
+            if (otomatikAtiwModu == false)
+            {
+                Console.WriteLine("Otomatik atiw modu aktiv edildi");
+            }
+            else
+            {
+                Console.WriteLine("Tekli atiw modu aktiv edildi");
+            }
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine($"{daraginGulleTutumu-daragdakiGulleSayi}");
+        }
+        public void Reload()
+        {
+            this.daragdakiGulleSayi = daraginGulleTutumu;
+            Console.WriteLine("Darag tam dolduruldu");
+        }
+    }
+}
